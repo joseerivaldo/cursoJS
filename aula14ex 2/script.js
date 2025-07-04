56e903ba6a087a552f3b63a92717b98621e9b737
José Erivaldo Meneses <67519141+joseerivaldo@users.noreply.github.com>
2025-07-04 10:27:40 -0300

    .

---
+++ aula14ex 2/script.js
@@ -0,0 +1,15 @@
+function tabuada(){
+    const numero = document.querySelector('input#numero')
+    const tab = document.querySelector('select#tabuada')
+
+    if(numero.value.length == 0){
+        alert('Preencha o campo "Número"')
+    }
+    else{
+        const n = Number(numero.value)
+        let c = 1;
+        while(c <= 10){
+            let item = document.createElement
+        }
+    }
+}
