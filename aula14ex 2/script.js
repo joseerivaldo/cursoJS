43689b45290f4372964ccdb57d2d4b0c8fbae5ce
José Erivaldo Meneses <67519141+joseerivaldo@users.noreply.github.com>
2025-07-04 15:23:22 -0300

    .

--- aula14ex 2/script.js
+++ aula14ex 2/script.js
@@ -7,9 +7,11 @@ function tabuada(){
     }
     else{
         const n = Number(numero.value)
-        let c = 1;
-        while(c <= 10){
-            let item = document.createElement
+        tab.innerHTML = ''
+        for(let c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
         }
     }
 }
