1ff12ec5d20e350a02740dbb14f092118ca4c1a1
José Erivaldo Meneses <67519141+joseerivaldo@users.noreply.github.com>
2025-07-04 09:30:16 -0300

    .

--- aula14ex/script.js
+++ aula14ex/script.js
@@ -1,5 +1,28 @@
 function contar(){
-    const inicio = document.querySelector('p#txti')
-    const fim = document.querySelector('p#txtf')
-    const passo = document.querySelector('p#txtp')
+    const inicio = document.querySelector('input#txti')
+    const fim = document.querySelector('input#txtf')
+    const passo = document.querySelector('input#txtp')
+    const res = document.querySelector('div#res')
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert('Preencha todos os campos')
+    }
+    else{
+        res.innerHTML = 'Contando: '
+        const i = Number(inicio.value)
+        const f = Number(fim.value)
+        const p = Number(passo.value)
+
+        //contagem crescente
+        if(i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} `
+            }
+            //contagem regressiva
+        }else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} `
+            }
+        }
+    }
 }
